08a555622662d6b9c1e45af8bcb37a4eaba95a54
ksviety <ksviety@gmail.com>
2020-10-05 08:55:47 +0500

    added intake model

---
+++ app/src/main/java/me/ksviety/nutrition_diary/data/model/Intake.kt
@@ -0,0 +1,19 @@
+package me.ksviety.nutrition_diary.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+
+/**
+ * @param timestamp Unix epoch seconds (by default set to now)
+ */
+@Entity(tableName = "intakes")
+data class Intake(
+		var name: String,
+		var calories: Float,
+		var isNecessary: Boolean,
+		var type: IntakeType,
+		@PrimaryKey(autoGenerate = true)
+		val id: Int = 0,
+		val timestamp: Long = Instant.now().epochSecond
+)
---
+++ app/src/main/java/me/ksviety/nutrition_diary/data/model/IntakeType.kt
@@ -0,0 +1,6 @@
+package me.ksviety.nutrition_diary.data.model
+
+enum class IntakeType {
+	Food,
+	Drink
+}
